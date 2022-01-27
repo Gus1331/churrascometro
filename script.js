8fce66eb1cd53d2a84d6dc2ed7b0cd147f640ab7
Gus Oliver <gustavooliver.souza@gmail.com>
2022-01-26 22:40:05 -0300

    html

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+    PROJETO CHURRASCÔMETRO
+
+*objetivo:
+    Calcular a quantidade de carne, dôces, bebidas e mesas/cadeiras referente ao número total de pessoas e tempo de duração do evento
+
+#base por pessoa:
+    >Adultos
+        400g de carne - 650g por mais de 6 horas
+        1200ml de bebida tipo "A" - 2000ml por mais de 6 horas
+        700ml de bebida tipo "B" - 900ml por mais de 6 horas
+        2 pacotes de docês com 6 unidades - 3 pacotes por mais de 6 horas
+    >Crianças
+        200g de carne - 325g por mais de 6 horas
+        crianças não podem ingerir bebida tipo "A" 
+        650ml de bebida tipo "B" - 800ml por mais de 6 horas
+        3 pacotes com 6 unidades - 3,5 por mais de 6 horas
+
+#mesas e cadeiras:
+    Cada pessoa ocupa 1 cadeira, sendo que a cada 4 pessoas, uma mesa é ocupada 
+    Obs: deixar sempre 3 mesas para colocar as comidas e bebidas e 3 cadeiras sobrando
+*/
+
